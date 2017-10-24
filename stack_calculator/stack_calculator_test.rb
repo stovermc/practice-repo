021f37f5e90a29df90489a9f125178d95d22829f
stovermc <stovermc@gmail.com>
2017-10-24 15:08:11 -0600

    Add method to sanitize instructions
    -Sanitize_instructions takes in the instruction string and splits it
    based on white space
     - It then converts any numbers and into integers.
    - It returns an new array of integers and instruction strings
    
    Fix assert statement in tests, add method for +
    
    - changed assert —> assert_equals where appropriate in test
    - add method removes last to numbers from stack, sums them and pushes
    the sum back to the stack
    
    Add subract method
    
    - subtract method removes last two integers from stack and subtracts
    the smaller value from the larger then pushes the difference back to
    the top of the stack.
    
    - refactored to have the stack values be integers. They are converted
    to strings when the solution is returned.
    
    Add duplicate method
    
    - duplicate pushes a duplicate of the last element in the stack to the
    top of the stack
    - added complex instructions to test —> Stack Calculator works!

--- stack_calculator/stack_calculator_test.rb
+++ stack_calculator/stack_calculator_test.rb
@@ -14,11 +14,50 @@ class StackCalculatorTest < Minitest::Test
   end
 
   def test_stack_upon_initialization
-    assert [], sc.stack
+    assert_equal [], sc.stack
   end
 
-  def test_integer_is_pushed_to_stack
-    sc.process('3')
-    assert ['3'], sc.stack
+  def test_sanitize_instructions
+    assert_equal [3, 'ADD'], sc.sanitize_instructions('3 ADD')
+  end
+
+  def test_process_returns_top_element_on_stack
+    assert_equal '5', sc.process('5 5')
+  end
+
+  def test_single_integer_is_pushed_to_stack
+    assert_equal '3', sc.process('3')
+  end
+
+  def test_multiple_integers_are_pushed_to_stack
+    sc.process('3 5')
+    assert_equal [3, 5], sc.stack
+  end
+
+  def test_add_removes_and_adds_last_two_integers
+    assert_equal '10', sc.process('5 5 +')
+  end
+
+  def test_subtract_removes_and_subracts_last_two_integers
+    assert_equal '0', sc.process('5 5 -')
+  end
+
+  def test_lower_value_is_subtracted_from_larger
+    assert_equal '3', sc.process('7 10 -')
+  end
+
+  def test_pop_removes_last_element_in_stack
+    assert_equal '3', sc.process('3 7 POP')
+  end
+
+  def test_duplicate_duplicates_last_element_on_stack
+    assert_equal '3', sc.process('3')
+    assert_equal 1, sc.stack.length
+    assert_equal '3', sc.process('DUP')
+    assert_equal 2, sc.stack.length
+  end
+
+  def test_complex_example
+    assert_equal '7', sc.process("13 DUP 4 POP 5 DUP + DUP + -")
   end
 end
